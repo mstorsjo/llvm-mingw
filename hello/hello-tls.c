fa5b73dc01d2017b5a69bb9331c7136402225e54
Martin Storsjö <martin@martin.st>
2018-01-15 10:56:25 +0200

    Speed up hello-tls.c by running fewer iterations and threads
    Also improve a printout.

--- hello/hello-tls.c
+++ hello/hello-tls.c
@@ -10,26 +10,26 @@ static __thread int tlsvar = 1;
 
 static unsigned __stdcall threadfunc(void* arg) {
 	int id = (int)arg;
-	printf("thread %d, tlsvar %p %d\n", id, &tlsvar, tlsvar);
+	printf("thread %d, tlsvar %p initially %d\n", id, &tlsvar, tlsvar);
 	tlsvar = id + 100;
-	for (int i = 0; i < 5; i++) {
+	for (int i = 0; i < 4; i++) {
 		printf("thread %d, tlsvar %p %d\n", id, &tlsvar, tlsvar);
 		tlsvar += 10;
-		Sleep(1000);
+		Sleep(500);
 	}
 	return 0;
 }
 
 int main(int argc, char* argv[]) {
-	HANDLE threads[5];
+	HANDLE threads[3];
 
-	for (int i = 0; i < 5; i++) {
+	for (int i = 0; i < 3; i++) {
 		printf("mainthread, tlsvar %p %d\n", &tlsvar, tlsvar);
 		tlsvar += 10;
 		threads[i] = (HANDLE)_beginthreadex(NULL, 0, threadfunc, (void*)(intptr_t) (i + 1), 0, NULL);
-		Sleep(750);
+		Sleep(350);
 	}
-	for (int i = 0; i < 5; i++) {
+	for (int i = 0; i < 3; i++) {
 		WaitForSingleObject(threads[i], INFINITE);
 		CloseHandle(threads[i]);
 	}
