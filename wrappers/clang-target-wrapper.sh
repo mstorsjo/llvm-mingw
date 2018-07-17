ca43ab5ccddc8830f2dca405702ff1913334cd8c
Martin Storsjö <martin@martin.st>
2018-07-17 22:26:54 +0300

    Use dwarf exceptions on i686 as well
    Now libunwind works properly in this case for dwarf on i686 mingw.

--- wrappers/clang-target-wrapper.sh
+++ wrappers/clang-target-wrapper.sh
@@ -10,10 +10,8 @@ esac
 ARCH=$(echo $TARGET | sed 's/-.*//')
 case $ARCH in
 i686)
-    # Dwarf is the default for i686, but currently there's an issue
-    # in libunwind with unwinding clang generated dwarf opcodes on 32 bit
-    # x86, pending resolution at https://reviews.llvm.org/D38680.
-    ARCH_FLAGS=-fsjlj-exceptions
+    # Dwarf is the default here.
+    ARCH_FLAGS=
     ;;
 x86_64)
     # Explicitly request dwarf on x86_64; SEH is the default there but
