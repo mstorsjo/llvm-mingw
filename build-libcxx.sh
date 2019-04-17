6b909e219bddb000618a34a0468c242746f2e53f
Martin Storsjö <martin@martin.st>
2019-04-18 00:50:33 +0300

    Build a separate static copy of libcxx for use with other CRT variants
    It should hopefully be enough with one copy for old msvcr* variants,
    and one for ucrt.

--- build-libcxx.sh
+++ build-libcxx.sh
@@ -4,6 +4,7 @@ set -e
 
 BUILD_STATIC=1
 BUILD_SHARED=1
+BUILD_OTHER_CRT=1
 
 while [ $# -gt 0 ]; do
     if [ "$1" = "--disable-shared" ]; then
@@ -14,13 +15,15 @@ while [ $# -gt 0 ]; do
         BUILD_STATIC=
     elif [ "$1" = "--enable-static" ]; then
         BUILD_STATIC=1
+    elif [ "$1" = "--disable-other-crt" ]; then
+        BUILD_OTHER_CRT=
     else
         PREFIX="$1"
     fi
     shift
 done
 if [ -z "$PREFIX" ]; then
-    echo $0 [--disable-shared] [--disable-static] dest
+    echo $0 [--disable-shared] [--disable-static] [--disable-other-crt] dest
     exit 1
 fi
 
@@ -77,6 +80,9 @@ esac
 
 build_all() {
     type="$1"
+    variant="$2"
+    extra_cflags="$3"
+    extra_ldflags="$4"
     if [ "$type" = "shared" ]; then
         SHARED=TRUE
         STATIC=FALSE
@@ -87,8 +93,15 @@ build_all() {
 
     cd libunwind
     for arch in $ARCHS; do
-        mkdir -p build-$arch-$type
-        cd build-$arch-$type
+        builddir="build-$arch-$type"
+        subdir=""
+        if [ -n "$variant" ]; then
+            builddir="$builddir-$variant"
+            subdir="/$variant"
+        fi
+
+        mkdir -p $builddir
+        cd $builddir
         cmake \
             ${CMAKE_GENERATOR+-G} "$CMAKE_GENERATOR" \
             -DCMAKE_BUILD_TYPE=Release \
@@ -111,9 +124,10 @@ build_all() {
             -DLIBUNWIND_ENABLE_STATIC=$STATIC \
             -DLIBUNWIND_ENABLE_CROSS_UNWINDING=FALSE \
             -DLIBUNWIND_STANDALONE_BUILD=TRUE \
-            -DCMAKE_CXX_FLAGS="-Wno-dll-attribute-on-redeclaration" \
-            -DCMAKE_C_FLAGS="-Wno-dll-attribute-on-redeclaration" \
-            -DCMAKE_SHARED_LINKER_FLAGS="-lpsapi" \
+            -DLIBUNWIND_LIBDIR_SUFFIX="$subdir" \
+            -DCMAKE_CXX_FLAGS="-Wno-dll-attribute-on-redeclaration $extra_flags" \
+            -DCMAKE_C_FLAGS="-Wno-dll-attribute-on-redeclaration $extra_cflags" \
+            -DCMAKE_SHARED_LINKER_FLAGS="-lpsapi $extra_ldflags" \
             ..
         make -j$CORES
         make install
@@ -124,7 +138,7 @@ build_all() {
             # Merge libpsapi.a into the static library libunwind.a, to
             # avoid having to specify -lpsapi when linking to it.
             llvm-ar qcsL \
-                $PREFIX/$arch-w64-mingw32/lib/libunwind.a \
+                $PREFIX/$arch-w64-mingw32/lib$subdir/libunwind.a \
                 $PREFIX/$arch-w64-mingw32/lib/libpsapi.a
         fi
         cd ..
@@ -133,8 +147,8 @@ build_all() {
 
     cd libcxxabi
     for arch in $ARCHS; do
-        mkdir -p build-$arch-$type
-        cd build-$arch-$type
+        mkdir -p $builddir
+        cd $builddir
         if [ "$type" = "shared" ]; then
             LIBCXXABI_VISIBILITY_FLAGS="-D_LIBCPP_BUILDING_LIBRARY= -U_LIBCXXABI_DISABLE_VISIBILITY_ANNOTATIONS"
         else
@@ -164,7 +178,7 @@ build_all() {
             -DLLVM_NO_OLD_LIBSTDCXX=TRUE \
             -DCXX_SUPPORTS_CXX11=TRUE \
             -DCXX_SUPPORTS_CXX_STD=TRUE \
-            -DCMAKE_CXX_FLAGS="$LIBCXXABI_VISIBILITY_FLAGS -D_LIBCPP_HAS_THREAD_API_WIN32" \
+            -DCMAKE_CXX_FLAGS="$LIBCXXABI_VISIBILITY_FLAGS -D_LIBCPP_HAS_THREAD_API_WIN32 $extra_cflags" \
             ..
         make -j$CORES
         cd ..
@@ -173,12 +187,17 @@ build_all() {
 
     cd libcxx
     for arch in $ARCHS; do
-        mkdir -p build-$arch-$type
-        cd build-$arch-$type
+        mkdir -p $builddir
+        cd $builddir
         if [ "$type" = "shared" ]; then
             LIBCXX_VISIBILITY_FLAGS="-D_LIBCXXABI_BUILDING_LIBRARY"
         else
             LIBCXX_VISIBILITY_FLAGS="-D_LIBCXXABI_DISABLE_VISIBILITY_ANNOTATIONS"
+        fi
+        if [ -n "$variant" ]; then
+            install_headers=FALSE
+        else
+            install_headers=TRUE
         fi
         cmake \
             ${CMAKE_GENERATOR+-G} "$CMAKE_GENERATOR" \
@@ -194,7 +213,7 @@ build_all() {
             -DCMAKE_AR=$PREFIX/bin/llvm-ar \
             -DCMAKE_RANLIB=$PREFIX/bin/llvm-ranlib \
             -DLIBCXX_USE_COMPILER_RT=ON \
-            -DLIBCXX_INSTALL_HEADERS=ON \
+            -DLIBCXX_INSTALL_HEADERS=$install_headers \
             -DLIBCXX_ENABLE_EXCEPTIONS=ON \
             -DLIBCXX_ENABLE_THREADS=ON \
             -DLIBCXX_HAS_WIN32_THREAD_API=ON \
@@ -208,11 +227,11 @@ build_all() {
             -DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=TRUE \
             -DLIBCXX_CXX_ABI=libcxxabi \
             -DLIBCXX_CXX_ABI_INCLUDE_PATHS=../../libcxxabi/include \
-            -DLIBCXX_CXX_ABI_LIBRARY_PATH=../../libcxxabi/build-$arch-$type/lib \
-            -DLIBCXX_LIBDIR_SUFFIX="" \
+            -DLIBCXX_CXX_ABI_LIBRARY_PATH=../../libcxxabi/$builddir/lib \
+            -DLIBCXX_LIBDIR_SUFFIX="$subdir" \
             -DLIBCXX_INCLUDE_TESTS=FALSE \
-            -DCMAKE_CXX_FLAGS="$LIBCXX_VISIBILITY_FLAGS" \
-            -DCMAKE_SHARED_LINKER_FLAGS="-lunwind -Wl,--export-all-symbols" \
+            -DCMAKE_CXX_FLAGS="$LIBCXX_VISIBILITY_FLAGS $extra_cflags" \
+            -DCMAKE_SHARED_LINKER_FLAGS="-lunwind -Wl,--export-all-symbols $extra_ldflags" \
             -DLIBCXX_ENABLE_ABI_LINKER_SCRIPT=FALSE \
             ..
         make -j$CORES
@@ -224,8 +243,8 @@ build_all() {
             cp lib/libc++.dll $PREFIX/$arch-w64-mingw32/bin
         else
             llvm-ar qcsL \
-                $PREFIX/$arch-w64-mingw32/lib/libc++.a \
-                $PREFIX/$arch-w64-mingw32/lib/libunwind.a
+                $PREFIX/$arch-w64-mingw32/lib$subdir/libc++.a \
+                $PREFIX/$arch-w64-mingw32/lib$subdir/libunwind.a
         fi
         cd ..
     done
@@ -236,3 +255,32 @@ build_all() {
 # work when linking against the DLL, but not vice versa.
 [ -z "$BUILD_SHARED" ] || build_all shared
 [ -z "$BUILD_STATIC" ] || build_all static
+
+if [ -n "$BUILD_OTHER_CRT" ]; then
+    cat<<EOF > is-ucrt.c
+    #include <_mingw.h>
+    #if __MSVCRT_VERSION__ < 0x1400
+    #error not ucrt
+    #endif
+EOF
+    ANY_ARCH=$(echo $ARCHS | awk '{print $1}')
+    if $ANY_ARCH-w64-mingw32-gcc -E is-ucrt.c > /dev/null 2>&1; then
+        CRT=ucrt
+        OTHERCRT=msvcrt
+        OTHERVER=0x700
+        OTHERLIB=msvcrt-os
+    else
+        CRT=msvcrt
+        OTHERCRT=ucrt
+        OTHERVER=0x1400
+        OTHERLIB=ucrt
+    fi
+    rm -f is-ucrt.c
+
+    for arch in $ARCHS; do
+        mkdir -p $PREFIX/$arch-w64-mingw32/lib/$CRT
+        cp $PREFIX/$arch-w64-mingw32/lib/libc++.a $PREFIX/$arch-w64-mingw32/lib/$CRT
+    done
+
+    [ -z "$BUILD_STATIC" ] || build_all static $OTHERCRT "-D__MSVCRT_VERSION__=$OTHERVER" "-l$OTHERLIB"
+fi
