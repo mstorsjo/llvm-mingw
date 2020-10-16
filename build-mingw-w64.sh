a83473f6e2a5ba4725e39aa4c966a520239693d1
Orgad Shaneh <orgad.shaneh@audiocodes.com>
2020-10-16 14:56:30 +0300

    build-mingw-w64: Clone on https instead of git protocol
    git port is typically blocked by outbound corporate firewalls.

--- build-mingw-w64.sh
+++ build-mingw-w64.sh
@@ -48,7 +48,7 @@ if [ -z "$CHECKOUT_ONLY" ]; then
 fi
 
 if [ ! -d mingw-w64 ]; then
-    git clone git://git.code.sf.net/p/mingw-w64/mingw-w64
+    git clone https://git.code.sf.net/p/mingw-w64/mingw-w64
     CHECKOUT=1
 fi
 
