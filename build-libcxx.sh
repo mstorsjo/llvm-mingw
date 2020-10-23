a507720befbc0b4fd0ac319b515eead40db35d1e
Martin Storsjö <martin@martin.st>
2020-10-23 10:06:17 +0300

    build-libcxx: Build libunwind+libcxxabi+libcxx with one single cmake build
    This simplifies their interdependencies quite a bit.

--- build-libcxx.sh
+++ build-libcxx.sh
@@ -53,6 +53,8 @@ cd llvm-project
 
 LLVM_PATH="$(pwd)/llvm"
 
+cd runtimes
+
 if [ -n "$(which ninja)" ]; then
     CMAKE_GENERATOR="Ninja"
     NINJA=1
@@ -74,15 +76,16 @@ fi
 
 build_all() {
     type="$1"
+    CMAKEFLAGS=""
     if [ "$type" = "shared" ]; then
         SHARED=TRUE
         STATIC=FALSE
+        CMAKEFLAGS="$CMAKEFLAGS -DLIBCXXABI_USE_LLVM_UNWINDER=ON"
     else
         SHARED=FALSE
         STATIC=TRUE
     fi
 
-    cd libunwind
     for arch in $ARCHS; do
         [ -z "$CLEAN" ] || rm -rf build-$arch-$type
         mkdir -p build-$arch-$type
@@ -100,38 +103,12 @@ build_all() {
             -DLLVM_PATH="$LLVM_PATH" \
             -DCMAKE_AR="$PREFIX/bin/llvm-ar" \
             -DCMAKE_RANLIB="$PREFIX/bin/llvm-ranlib" \
+            -DLLVM_ENABLE_RUNTIMES="libunwind;libcxxabi;libcxx" \
             -DLIBUNWIND_USE_COMPILER_RT=TRUE \
             -DLIBUNWIND_ENABLE_THREADS=TRUE \
             -DLIBUNWIND_ENABLE_SHARED=$SHARED \
             -DLIBUNWIND_ENABLE_STATIC=$STATIC \
             -DLIBUNWIND_ENABLE_CROSS_UNWINDING=FALSE \
-            ..
-        $BUILDCMD ${CORES+-j$CORES}
-        $BUILDCMD install
-        cd ..
-    done
-    cd ..
-
-    # Configure, but don't build, libcxx, so that libcxxabi has
-    # proper headers to refer to
-    cd libcxx
-    for arch in $ARCHS; do
-        [ -z "$CLEAN" ] || rm -rf build-$arch-$type
-        mkdir -p build-$arch-$type
-        cd build-$arch-$type
-        cmake \
-            ${CMAKE_GENERATOR+-G} "$CMAKE_GENERATOR" \
-            -DCMAKE_BUILD_TYPE=Release \
-            -DCMAKE_INSTALL_PREFIX="$PREFIX/$arch-w64-mingw32" \
-            -DCMAKE_C_COMPILER=$arch-w64-mingw32-clang \
-            -DCMAKE_CXX_COMPILER=$arch-w64-mingw32-clang++ \
-            -DCMAKE_CROSSCOMPILING=TRUE \
-            -DCMAKE_SYSTEM_NAME=Windows \
-            -DCMAKE_C_COMPILER_WORKS=TRUE \
-            -DCMAKE_CXX_COMPILER_WORKS=TRUE \
-            -DCMAKE_AR="$PREFIX/bin/llvm-ar" \
-            -DCMAKE_RANLIB="$PREFIX/bin/llvm-ranlib" \
-            -DLLVM_PATH="$LLVM_PATH" \
             -DLIBCXX_USE_COMPILER_RT=ON \
             -DLIBCXX_INSTALL_HEADERS=ON \
             -DLIBCXX_ENABLE_EXCEPTIONS=ON \
@@ -143,54 +120,18 @@ build_all() {
             -DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=TRUE \
             -DLIBCXX_ENABLE_NEW_DELETE_DEFINITIONS=OFF \
             -DLIBCXX_CXX_ABI=libcxxabi \
-            -DLIBCXX_CXX_ABI_INCLUDE_PATHS=../../libcxxabi/include \
-            -DLIBCXX_CXX_ABI_LIBRARY_PATH=../../libcxxabi/build-$arch-$type/lib \
             -DLIBCXX_LIBDIR_SUFFIX="" \
             -DLIBCXX_INCLUDE_TESTS=FALSE \
-            -DCMAKE_SHARED_LINKER_FLAGS="-lunwind" \
             -DLIBCXX_ENABLE_ABI_LINKER_SCRIPT=FALSE \
-            ..
-        $BUILDCMD ${CORES+-j$CORES} generate-cxx-headers
-        cd ..
-    done
-    cd ..
-
-    cd libcxxabi
-    for arch in $ARCHS; do
-        [ -z "$CLEAN" ] || rm -rf build-$arch-$type
-        mkdir -p build-$arch-$type
-        cd build-$arch-$type
-        cmake \
-            ${CMAKE_GENERATOR+-G} "$CMAKE_GENERATOR" \
-            -DCMAKE_BUILD_TYPE=Release \
-            -DCMAKE_INSTALL_PREFIX="$PREFIX/$arch-w64-mingw32" \
-            -DCMAKE_C_COMPILER=$arch-w64-mingw32-clang \
-            -DCMAKE_CXX_COMPILER=$arch-w64-mingw32-clang++ \
-            -DCMAKE_CROSSCOMPILING=TRUE \
-            -DCMAKE_SYSTEM_NAME=Windows \
-            -DCMAKE_C_COMPILER_WORKS=TRUE \
-            -DCMAKE_CXX_COMPILER_WORKS=TRUE \
-            -DLLVM_PATH="$LLVM_PATH" \
-            -DCMAKE_AR="$PREFIX/bin/llvm-ar" \
-            -DCMAKE_RANLIB="$PREFIX/bin/llvm-ranlib" \
             -DLIBCXXABI_USE_COMPILER_RT=ON \
             -DLIBCXXABI_ENABLE_EXCEPTIONS=ON \
             -DLIBCXXABI_ENABLE_THREADS=ON \
             -DLIBCXXABI_ENABLE_SHARED=OFF \
-            -DLIBCXXABI_LIBCXX_INCLUDES=../../libcxx/build-$arch-$type/include/c++/v1 \
             -DLIBCXXABI_LIBDIR_SUFFIX="" \
             -DLIBCXXABI_ENABLE_NEW_DELETE_DEFINITIONS=ON \
-            -DLIBCXX_ENABLE_SHARED=$SHARED \
-            -DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=TRUE \
+            $CMAKEFLAGS \
             ..
-        $BUILDCMD ${CORES+-j$CORES}
-        cd ..
-    done
-    cd ..
 
-    cd libcxx
-    for arch in $ARCHS; do
-        cd build-$arch-$type
         $BUILDCMD ${CORES+-j$CORES}
         $BUILDCMD install
         if [ "$type" = "shared" ]; then
@@ -204,7 +145,6 @@ build_all() {
         fi
         cd ..
     done
-    cd ..
 }
 
 # Build shared first and static afterwards; the headers for static linking also
