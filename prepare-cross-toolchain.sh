733b3e875e2ca34ed8779b99bd66899dc462825d
Martin Storsjö <martin@martin.st>
2022-03-29 09:24:05 +0300

    WIP: Dockerfile.cross: Skip parts other than building the LLVM toolchain itself
    We still need to include the shared libc++.dll and libunwind.dll
    from the cross toolchain though.

--- prepare-cross-toolchain.sh
+++ prepare-cross-toolchain.sh
@@ -35,13 +35,3 @@ for i in libc++ libunwind; do
         cp $SRC/$CROSS_ARCH-w64-mingw32/bin/$i.dll $DEST/bin
     fi
 done
-
-cp -a $SRC/lib/clang/$CLANG_VERSION/lib $DEST/lib/clang/$CLANG_VERSION
-rm -rf $DEST/include
-cp -a $SRC/generic-w64-mingw32/include $DEST/include
-for arch in $ARCHS; do
-    mkdir -p $DEST/$arch-w64-mingw32
-    for subdir in bin lib; do
-        cp -a $SRC/$arch-w64-mingw32/$subdir $DEST/$arch-w64-mingw32
-    done
-done
