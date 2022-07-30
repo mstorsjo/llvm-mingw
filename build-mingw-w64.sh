fffe23a54933adb1fc0c79e1b6e8cf635af2fa2a
Martin Storsjö <martin@martin.st>
2022-07-31 01:08:27 +0300

    build-mingw-w64: Update to a newer version
    Notable changes are:
    - Fix building for armv7 with SEH enabled
    - Implement _set_com_error_handler and _com_raise_error in comdef.h
    - Add import libraries for older msvcrXX.dll versions
    - Redirect access() to __mingw_access() on UCRT
    - Assorted updates to Windows APIs

--- build-mingw-w64.sh
+++ build-mingw-w64.sh
@@ -18,7 +18,7 @@ set -e
 
 : ${DEFAULT_WIN32_WINNT:=0x601}
 : ${DEFAULT_MSVCRT:=ucrt}
-: ${MINGW_W64_VERSION:=a3f6d363d534e9d5563a0b7c677889101e6bbd42}
+: ${MINGW_W64_VERSION:=d0dec04461345121f7079d62e07f036ca7446c6c}
 
 while [ $# -gt 0 ]; do
     case "$1" in
