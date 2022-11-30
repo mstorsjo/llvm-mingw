c18225c0d0e34f5be0a396051615c1584d245f28
Martin Storsjö <martin@martin.st>
2022-11-30 13:33:04 +0200

    build-make: Bump to GNU make 4.4.1
    Versions 4.3 and above only come packaged as .tar.gz and .tar.lz,
    and the lzip decompressor is less commonly readily available, thus
    switch back to the .tar.gz format.

--- build-make.sh
+++ build-make.sh
@@ -18,7 +18,7 @@ set -e
 
 unset HOST
 
-: ${MAKE_VERSION:=4.2.1}
+: ${MAKE_VERSION:=4.4.1}
 
 while [ $# -gt 0 ]; do
     case "$1" in
@@ -53,8 +53,8 @@ download() {
 }
 
 if [ ! -d make-$MAKE_VERSION ]; then
-    download https://ftp.gnu.org/gnu/make/make-$MAKE_VERSION.tar.bz2
-    tar -jxf make-$MAKE_VERSION.tar.bz2
+    download https://ftp.gnu.org/gnu/make/make-$MAKE_VERSION.tar.gz
+    tar -zxf make-$MAKE_VERSION.tar.gz
 fi
 
 cd make-$MAKE_VERSION
