5ca3fc332fea168233c8971f4f23cc21a88f46e6
Martin Storsjö <martin@martin.st>
2024-05-19 00:23:15 +0300

    build-mingw-w64: Bump to the version as of May 18th
    This version fixes some cases of duplicate time.h related symbols,
    in some build configurations (an issue only present in the
    previous snapshot). This version also uses the right format attributes
    for printf/scanf functions for Clang, enabling format string warnings
    when using e.g. the mingw ansi stdio routines.

--- build-mingw-w64.sh
+++ build-mingw-w64.sh
@@ -18,7 +18,7 @@ set -e
 
 : ${DEFAULT_WIN32_WINNT:=0x601}
 : ${DEFAULT_MSVCRT:=ucrt}
-: ${MINGW_W64_VERSION:=8fdf7c9b5c0d7fe3e01ee8cff13fac813f30d7be}
+: ${MINGW_W64_VERSION:=dbda83ce39374428f82af7294c876f49d5921483}
 
 CFGUARD_FLAGS="--enable-cfguard"
 
