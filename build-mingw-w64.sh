d09d06ebafac5708e9a6cf62db6c89f6664ccc76
Martin Storsjö <martin@martin.st>
2024-06-25 13:13:21 +0300

    WIP: build-mingw-w64: Apply patches to be tested

--- build-mingw-w64.sh
+++ build-mingw-w64.sh
@@ -65,6 +65,7 @@ cd mingw-w64
 if [ -n "$SYNC" ] || [ -n "$CHECKOUT" ]; then
     [ -z "$SYNC" ] || git fetch
     git checkout $MINGW_W64_VERSION
+    git am -3 ../patches/mingw-w64/*.patch
 fi
 
 [ -z "$CHECKOUT_ONLY" ] || exit 0
