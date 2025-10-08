e6cbbe9a352fe6da6ba587fa4f6317f0701fdf5a
Martin Storsjö <martin@martin.st>
2025-10-08 23:43:05 +0300

    test: Add a test for catching pointers by reference
    This test should make sure that
    https://github.com/mstorsjo/llvm-mingw/issues/522 doesn't
    regress again.

---
+++ test/exception-catch-ptr-ref.cpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2025 Martin Storsjo
+ *
+ * This file is part of llvm-mingw.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+    try {
+        const char *e = "EX";
+        printf("throwing e %p &e %p\n", e, &e);
+        throw e;
+    } catch (const char* &e) {
+        printf("caught e %p &e %p\n", e, &e);
+        printf("e=%s\n", e);
+        if (strcmp(e, "EX")) {
+            printf("Incorrect string pointer reference caught!\n");
+            return 1;
+        }
+    }
+    return 0;
+}
