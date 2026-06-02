0272df2cc108a3392d5dcf1cc20c3b48910b08af
Martin Storsjö <martin@martin.st>
2026-06-02 12:20:07 +0300

    build-llvm: Bump to LLVM 22.1.7

--- build-llvm.sh
+++ build-llvm.sh
@@ -17,7 +17,7 @@
 set -e
 
 : ${LLVM_REPOSITORY:=https://github.com/llvm/llvm-project.git}
-: ${LLVM_VERSION:=llvmorg-22.1.6}
+: ${LLVM_VERSION:=llvmorg-22.1.7}
 ASSERTS=OFF
 unset HOST
 BUILDDIR="build"
