975930b5b7f8a0a594de179825eae84b674c51bd
Martin Storsjö <martin@martin.st>
2026-07-28 18:44:59 +0300

    Revert "Add ARM64X support"
    This reverts commit 64effa9a71d55e9e14533c8efb63098c6e03a3ff.
    
    It turns out that the LLD handling of the TLS data structures
    ("_tls_used" and "_tls_index") needs more work, causing the
    arm64ec side of arm64x binaries to not get them hooked up properly.

--- build-openmp.sh
+++ build-openmp.sh
@@ -64,25 +64,11 @@ else
     esac
 fi
 
-ARM64X_FLAGS=""
-for arch in $ARCHS; do
-    case $arch in
-    arm64ec) ARM64X_FLAGS="-marm64x" ;;
-    esac
-done
-
 for arch in $ARCHS; do
     CMAKEFLAGS=""
-    FLAGS=""
     case $arch in
     x86_64)
         CMAKEFLAGS="$CMAKEFLAGS -DLIBOMP_ASMFLAGS=-m64"
-        ;;
-    aarch64)
-        FLAGS="$ARM64X_FLAGS"
-        ;;
-    arm64ec)
-        continue
         ;;
     esac
 
@@ -104,9 +90,8 @@ for arch in $ARCHS; do
         -DCMAKE_RANLIB="$PREFIX/bin/llvm-ranlib" \
         -DLLVM_ENABLE_RUNTIMES="openmp" \
         -DLIBOMP_ENABLE_SHARED=TRUE \
-        -DCMAKE_C_FLAGS_INIT="$CFGUARD_CFLAGS $FLAGS" \
-        -DCMAKE_CXX_FLAGS_INIT="$CFGUARD_CFLAGS $FLAGS" \
-        -DCMAKE_SHARED_LINKER_FLAGS="$FLAGS" \
+        -DCMAKE_C_FLAGS_INIT="$CFGUARD_CFLAGS" \
+        -DCMAKE_CXX_FLAGS_INIT="$CFGUARD_CFLAGS" \
         $CMAKEFLAGS \
         ..
     cmake --build . ${CORES:+-j${CORES}}
