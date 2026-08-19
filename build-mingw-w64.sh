649d88112d3ef4be65101e51a7c8e828dc267258
Martin Storsjö <martin@martin.st>
2026-08-19 11:33:51 +0300

    WIP: Restore -Oz back to -O2

--- build-mingw-w64.sh
+++ build-mingw-w64.sh
@@ -147,7 +147,7 @@ for arch in $ARCHS; do
     [ -z "$CLEAN" ] || rm -rf build-$arch
     mkdir -p build-$arch
     cd build-$arch
-    ../configure --host=$arch-w64-mingw32 --prefix="$PREFIX/$arch-w64-mingw32" $FLAGS $CFGUARD_FLAGS $CRT_CONFIG_FLAGS CFLAGS="-g -Oz"
+    ../configure --host=$arch-w64-mingw32 --prefix="$PREFIX/$arch-w64-mingw32" $FLAGS $CFGUARD_FLAGS $CRT_CONFIG_FLAGS CFLAGS="-g -O2"
     $MAKE -j$CORES
     $MAKE install
     cd ..
